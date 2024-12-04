90246fe9cab67a0e28964ebd733704b62cd9603b
5HoSaZ <ttzoththd@gmail.com>
2024-12-04 14:19:13 +0700

    add input option

--- reader_writer.py
+++ reader_writer.py
@@ -13,16 +13,14 @@ def write_process(name: str, shm: SharedMemory):
     writer.mainloop()
 
 
-def main():
+def main(num_reader: int, num_writer: int):
     sh_memory = SharedMemory(65)
-    NUM_READER = 1
-    NUM_WRITER = 1
-    for i in range(1, NUM_READER + 1):
+    for i in range(1, num_reader: + 1):
         name = f"Reader {i}"
         rproc = Process(target=read_process, args=(name, sh_memory))
         rproc.start()
     write_processes: list[Process] = []
-    for i in range(1, NUM_WRITER + 1):
+    for i in range(1, num_writer + 1):
         name = f"Writer {i}"
         wproc = Process(target=write_process, args=(name, sh_memory))
         wproc.start()
@@ -33,4 +31,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    num_reader = int(input("Input for number of readers: "))
+    num_writer = int(input("Input for number of writers: "))
+    main(num_reader, num_writer)
